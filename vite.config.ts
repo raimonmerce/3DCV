7b1d4dc4d1ed8a7f49f2a7b21c7e892f56b50fc2
RaimonMerceTMRW <raimon.merce@tmrw.com>
2025-03-29 00:52:07 +0900

    remove path to there

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/3DCV/", 
+  base: "/", 
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
